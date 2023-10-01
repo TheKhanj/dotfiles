07f96cbb7085b4e66d5e09e51d1799e5f295b661
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-10-01 14:50:33 +0330

    fix setup_fonts script

--- desktop/setup/setup_fonts.sh
+++ desktop/setup/setup_fonts.sh
@@ -2,9 +2,6 @@
 
 . $HOME/dotfiles/shell/.config/shell/rc.sh
 
-if [ hostname != 'peach' ]; then
-  return
-fi
-
 as_root rm -rf /etc/fonts/
-as_root cp fonts/* /etc/fonts/
+as_root mkdir /etc/fonts
+as_root cp -r fonts/* /etc/fonts/
